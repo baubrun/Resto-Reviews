f41dd5fdba7fe8d4d246f6ef5755499c5f3579b5
baubrun <baubelf@gmail.com>
2020-12-24 13:14:06 -0500

    added material-ui

---
+++ client/src/components/Reviews.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import MaterialTable, { MTableToolbar } from "material-table";
+import Grid from "@material-ui/core/Grid";
+import Typography from "@material-ui/core/Typography";
+import Box from "@material-ui/core/Box";
+
+
+const Reviews = () => {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default Reviews
