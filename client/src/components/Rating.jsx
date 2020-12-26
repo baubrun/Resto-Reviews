e7e7f248513bed11f6413b53b3480af4bd638818
baubrun <baubelf@gmail.com>
2020-12-25 21:14:23 -0500

    added rating

--- client/src/components/Rating.jsx
+++ client/src/components/Rating.jsx
@@ -1,20 +1,23 @@
 import React from "react";
 import Box from "@material-ui/core/Box";
 
-
 const style = {
-    color: "#fff",
-    margin: "8px 0 0 8px",
-    letterSpacing: "4px"
-}
-
+  color: "#fff",
+  margin: "8px 0 0 8px",
+  letterSpacing: "4px",
+};
 
 const Rating = (props) => {
   const stars = [];
 
-  for (let i = 0; i < 5; i++) {
+  for (let i = 1; i <= 5; i++) {
     if (i <= props.rating) {
       stars.push(<i class="fas fa-star"></i>);
+    } else if (
+      i === Math.ceil(props.rating) &&
+      !Number.isInteger(props.rating)
+    ) {
+      stars.push(<i class="fas fa-star-half-alt"></i>);
     } else {
       stars.push(<i class="far fa-star"></i>);
     }
